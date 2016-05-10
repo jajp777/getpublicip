689c9b0aacb7786da42b4d21c611d9dc5ee7e239
Daniel Ferreira <daniel@ferreira.fm>
2016-05-10 10:55:02 +0200

    Get IP address from REST API

--- Get-PublicIPAddress.ps1
+++ Get-PublicIPAddress.ps1
@@ -1,7 +1,11 @@
 function Get-PublicIPAddress {
-    process {
-        $returnobject = @{ Ip = $null }
-
-        New-Object -Property $returnobject -TypeName psobject
+    process {       
+        # Define URL to get public IP address:
+        $url = "https://api.ipify.org/?format=json"
+        
+        # Define return psobject and get public IP address:
+        $returnobject = New-Object -TypeName PSObject
+        $returnobject | Add-Member -MemberType NoteProperty -Name Datetime -Value (Get-Date)
+        $returnobject | Add-Member -MemberType NoteProperty -Name IP -Value (Invoke-RestMethod -Uri $url).ip -PassThru
     }
 }
